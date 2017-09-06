2e875c73c59330ebaf3eb6d152441737aab49d8d
falrnd <falrndTwitter@gmail.com>
2017-09-06 18:22:41 +0900

    ﻿ラジアンを排除した

--- SchoolFestival2017/Main.cpp
+++ SchoolFestival2017/Main.cpp
@@ -1,28 +1,35 @@
 ﻿#include<Siv3D.hpp>
 
 class Drag{
-public:
+private:
 	Point clicked;
+public:
 	void update(){
 		if(Input::MouseL.clicked){
 			clicked=Mouse::Pos();
 		}
 	}
-	Point from() const{return clicked;};
-	Point to() const{return Mouse::Pos();};
-	int32 duration(){return Input::MouseL.pressedDuration;};
-	double length(){return from().distanceFrom(to());};
-	double radian(){return Atan2((double)(to().y-from().y),to().x-from().x);};
-
-	bool moved(){return from()!=to();};
+	Point	from() const{return clicked;};
+	Point	to() const{return Mouse::Pos();};
+	int32	duration(){return Input::MouseL.pressedDuration;};
+	double	length(){return from().distanceFrom(to());};
+	double	radian(){ return Atan2((double)(to().y-from().y), to().x-from().x); };
+	Vec2	direction(){ return Vec2(to()-from()).normalize(); };
+	bool	moved(){return from()!=to();};
 };
 
-inline Line getLine(const Point& base,double length,double radian){
-	return Line(base,{base.x+length*cos(radian),base.y+length*sin(radian)});
+/*
+inline Line getLine(const Vec2& base, double length, double radian){
+	return Line(base, {base.x+length*cos(radian),base.y+length*sin(radian)});
+}
+*/
+inline Line getLine(const Vec2& base, double length, const Vec2& direction){
+	return Line(base, base+Vec2(direction).setLength(length));
 }
 
 class Unit{
-	static constexpr double friction=-4.0/60;
+private:
+	static constexpr double friction=-4.5/60;
 public:
 	Vec2 pos,speed{0,0};
 	Unit(const Vec2& pos={0,0})
@@ -30,19 +37,33 @@ public:
 	};
 	void update(){
 		pos.moveBy(speed);
-		const double dir=Atan2(speed.y,speed.x);
+		/*
+		const double dir=Atan2(speed.y, speed.x);
+		const double dx=cos(dir)*-friction,dy=sin(dir)*-friction;
+		LOG_DEBUG(dir);
+		*/
+		if(!speed.isZero()){
+			const Vec2 dir=speed.normalized();
+			const double dx=dir.x*-friction, dy=dir.y*-friction;
+			LOG_DEBUG(dir);
 
-		if(Abs(speed.x)<cos(dir)*-friction)
-			speed.x=0;
-		else
-			speed.x+=cos(dir)*friction;
-		if(Abs(speed.y)<sin(dir)*-friction)
-			speed.y=0;
-		else
-			speed.y+=sin(dir)*friction;
+			if(Abs(speed.x)<dx)
+				speed.x=0;
+			else
+				speed.x-=dx;
+			if(Abs(speed.y)<dy)
+				speed.y=0;
+			else
+				speed.y-=dy;
+		}
 	}
-	void accel(double v,double radian){
-		speed.moveBy(cos(radian)*v,sin(radian)*v);
+	/*
+	void accel(double v, double radian){
+		speed.moveBy(cos(radian)*v, sin(radian)*v);
+	}
+	*/
+	void accel(double v, const Vec2& direction){
+		speed.moveBy(Vec2(direction).setLength(v));
 	}
 };
 
@@ -62,7 +83,7 @@ void Main(){
 		drag.update();
 		unit.update();
 
-		debug(drag.radian()).draw(0,0);
+		debug(drag.direction()).draw(0, 0);
 		debug(unit.pos).draw(0,20);
 		debug(unit.speed).draw(0,40);
 		if(Input::MouseL.clicked){
@@ -70,16 +91,16 @@ void Main(){
 			powercircleease.start();
 		}
 		if(Input::MouseL.pressed){
-			getLine(drag.from(),drag.length()+3.2,drag.radian())
+			getLine(drag.from(), drag.length()+3.2, drag.direction())
 				.drawArrow( 9.0,{15.0,15.0},Palette::Black);
-			getLine(drag.from(),drag.length()    ,drag.radian())
+			getLine(drag.from(), drag.length()    , drag.direction())
 				.drawArrow( 5.0,{10.0,10.0},Palette::White);
 			Circle(drag.from(),powercircleease.easeOut()+drag.duration()/120.0)
 				.drawShadow({0,0},10,3,Palette::Black)
 				.draw(Palette::White);
 		}
 		if(Input::MouseL.released&&drag.moved()){
-			unit.accel(drag.duration()/3.0/60.0,drag.radian());
+			unit.accel(drag.duration()/3.0/60.0, drag.direction());
 		}
 		Circle(unit.pos,10).draw(Palette::Red);
 	}
