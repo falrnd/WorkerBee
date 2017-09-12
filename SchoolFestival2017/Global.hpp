cc2e0cae680d4de2b63b9f16e3df59b5fd2b29db
falrnd <falrndTwitter@gmail.com>
2017-09-12 18:58:48 +0900

    種&花のアセット名の配列を作成

--- SchoolFestival2017/Global.hpp
+++ SchoolFestival2017/Global.hpp
@@ -7,52 +7,67 @@ enum class Scene{
 	Result,
 	Ranking
 };
-struct GlobalData{
-
+struct Object{
+	Point pos;
+	int id;
+};
+class GlobalData{
+	const int seedslimit=3;
+	int score;
+	Array<Object>seeds,flowers;
 };
 using MySceneManager=SceneManager<Scene,GlobalData>;
 
 namespace Name{
 	const String BG		=L"BG";
-	const String nuts	=L"nuts";
-	const String kyuukon=L"kyuukon";
 	const String bee	=L"bee";
+	namespace Seed{
+		const String nuts	=L"nuts";
+		const String kyuukon=L"kyuukon";
+		const Array<String> colors={
+			nuts,
+			kyuukon
+		};
+	}
 	namespace Flower{
-		const String blue	=L"flower_blue";
-		const String brown	=L"flower_brown";
-		const String green	=L"flower_green";
+		const String blue	=L"flower_blue"  ;
+		const String brown	=L"flower_brown" ;
+		const String green	=L"flower_green" ;
 		const String orange	=L"flower_orange";
-		const String pink	=L"flower_pink";
+		const String pink	=L"flower_pink"  ;
 		const String purple	=L"flower_purple";
-		const String red	=L"flower_red";
-		const String sky	=L"flower_sky";
+		const String red	=L"flower_red"   ;
+		const String sky	=L"flower_sky"   ;
 		const String yellow	=L"flower_yellow";
+		const Array<String> colors={
+			blue,
+			brown,
+			green,
+			orange,
+			pink,
+			purple,
+			red,
+			sky,
+			yellow
+		};
 	}
 }
 
-const String root=L"resources/";
-inline bool _register(const String& v){
-	return TextureAsset::Register(v,root+L"small_"+v+L".png");
+inline bool _registerflower(const String& v){
+	return TextureAsset::Register(v,L"resources/small_"+v+L".png");
 }
 
 bool registerAssets(){
 	bool error=false;
 
-	error|=!FontManager::Register(root+L"nekokaburi.otf");
+	error|=!FontManager::Register(L"resources/nekokaburi.otf");
 	error|=!FontAsset::Register(L"font",40,L"‚Ë‚±‚©‚Ô‚è");
-	error|=!_register(Name::Flower::blue);
-	error|=!_register(Name::Flower::brown);
-	error|=!_register(Name::Flower::green);
-	error|=!_register(Name::Flower::orange);
-	error|=!_register(Name::Flower::pink);
-	error|=!_register(Name::Flower::purple);
-	error|=!_register(Name::Flower::red);
-	error|=!_register(Name::Flower::sky);
-	error|=!_register(Name::Flower::yellow);
-	error|=!TextureAsset::Register(Name::BG,		root+L"background.png");
-	error|=!TextureAsset::Register(Name::nuts,		root+L"nuts_himawari_single.png");
-	error|=!TextureAsset::Register(Name::kyuukon,	root+L"flower_tulip_kyuukon_single.png");
-	error|=!TextureAsset::Register(Name::bee,		root+L"mitsubachi_new.png");
+	for(auto c:Name::Flower::colors)
+		error|=!_registerflower(c);
+	error|=!TextureAsset::Register(Name::BG,			L"resources/background.png");
+	error|=!TextureAsset::Register(Name::Seed::nuts,	L"resources/nuts_himawari_single.png");
+	error|=!TextureAsset::Register(Name::Seed::kyuukon,	L"resources/flower_tulip_kyuukon_single.png");
+	error|=!TextureAsset::Register(Name::bee,			L"resources/mitsubachi_new.png");
 
 	return !error;
 }
